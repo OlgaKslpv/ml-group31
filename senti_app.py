50e327d1a2090eeee5930017e399d013b6ea8490
Tatiana Filimonova <virtroll@yandex.ru>
2022-11-29 05:38:04 +0300

    Added web apps

---
+++ senti_app.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+import streamlit as st
+
+classifier = pipeline("sentiment-analysis",   
+                      "blanchefort/rubert-base-cased-sentiment")
+
+st.write(classifier(st.text_input('Введите фразу для оценки')))
