64b57e2ed96114d4ec300692204f29e69915057f
Victor-Gmyrin <118006933+Victor-Gmyrin@users.noreply.github.com>
2023-01-20 21:48:33 +0300

    Update first.py

--- first.py
+++ first.py
@@ -2,9 +2,9 @@ import io
 import streamlit as st
 import numpy as np
 from transformers import pipeline
-#from tensorflow.keras.applications import EfficientNetB0
-#from tensorflow.keras.saving.hdf5_format import 
-#from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.saving.hdf5_format import 
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 #Проверка теста
 
 classifier = pipeline("sentiment-analysis",
