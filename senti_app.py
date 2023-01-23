ae7389c81b2487609a969d99647f49c1a6285832
Victor-Gmyrin <118006933+Victor-Gmyrin@users.noreply.github.com>
2023-01-23 16:24:11 +0300

    Update senti_app.py

--- senti_app.py
+++ senti_app.py
@@ -4,4 +4,4 @@ import streamlit as st
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
-st.write(classifier(st.text_input('Введите фразу для оценки')))
+st.write(classifier(st.text_input('Введите фразу для оценки...')))
