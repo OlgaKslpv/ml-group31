4e59b33397587793fbe79763b3349a1cd6cf46dd
Victor-Gmyrin <118006933+Victor-Gmyrin@users.noreply.github.com>
2023-01-24 18:50:13 +0300

    Update senti.py

--- senti.py
+++ senti.py
@@ -1,6 +1,17 @@
+from fastapi import FastAPI
 from transformers import pipeline
+from pydantic import BaseModel
 
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
+class Item(BaseModel):
+    text: str
 
-classifier("Я обожю инженерию машинного обучения!")
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict/")
+def predict(item: Item):
+    return classifier(item.text)[0]
