a43d68eb12f10e6fc50f3cbc300c712ddf62339a
Victor-Gmyrin <118006933+Victor-Gmyrin@users.noreply.github.com>
2023-01-24 18:58:27 +0300

    Update senti.py

--- senti.py
+++ senti.py
@@ -8,10 +8,6 @@ class Item(BaseModel):
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
