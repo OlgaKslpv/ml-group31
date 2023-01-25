e591bd1c03f681d29ca07a128479bb6ebecadf6a
KrupskiiAndrei <117271890+KrupskiiAndrei@users.noreply.github.com>
2023-01-25 22:00:49 +0300

    Update senti_app.py

--- senti_app.py
+++ senti_app.py
@@ -6,4 +6,5 @@ app = FastAPI()
 classifier = pipeline("sentiment-analysis",   
                       "blanchefort/rubert-base-cased-sentiment")
 
+
 st.write(classifier(st.text_input('Введите фразу для оценки...')))
