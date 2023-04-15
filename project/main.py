19dcd2a41e6c22b331867d1ef3b207fa9301d19d
OlgaKslpv <118008761+OlgaKslpv@users.noreply.github.com>
2023-04-15 11:09:07 +0300

    Update main.py
    Добавлен класс User и метод, который возвращает пользователя по его ID

--- project/main.py
+++ project/main.py
@@ -3,6 +3,12 @@ from transformers import pipeline
 from pydantic import BaseModel
 from fastapi import HTTPException
 from typing import List, Dict, Union
+from dataclasses import dataclass
+
+@dataclass
+class User:
+    id: int
+    email: str
 
 class Item(BaseModel):
     text: str
@@ -14,6 +20,8 @@ def postprocess(result: List[Dict[str, Union[str, float]]]) -> Dict[str, Union[s
     return result[0]
 
 app = FastAPI()
+
+user_repository = {1: User(1,"jjj@hhh.com"), 2: User(2, "ggg@bbb.com")}
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
@@ -31,3 +39,7 @@ def predict(item: Item):
     processed_text = preprocess(item.text)
     raw_result = classifier(processed_text)
     return postprocess(raw_result)
+
+@app.get("/get-user")
+def get_user_by_id(user_id: int):
+    return {"user": user_repository[user_id]}
